e29b16f886c4fa1543cf99f24a83dbcabdf4978f
Camilo Gomez <cagomeznu@gmai.com>
2017-04-10 19:21:17 -0300

    cambia profiling y añade discusion de resultados

--- logs/logs_all/plot_profiling.sh
+++ logs/logs_all/plot_profiling.sh
@@ -4,8 +4,8 @@ gnuplot -persist <<-EOFMarker
 	set terminal pngcairo enhanced font "arial,18" fontscale 1.0 size 1920,1080
 	set size ratio 1 1
 	
-	set title 'Code Profiling with some participants sending (Room of 30)'
-	set xlabel "Number of Messages"
+	set title 'Code Profiling with all participants sending'
+	set xlabel "Number of Participants"
 	set ylabel '% of Total Time'
 	
 	set key invert reverse Left outside
@@ -14,10 +14,26 @@ gnuplot -persist <<-EOFMarker
 	set xtics 1
 	
 	set style data histograms
-	
+	set style histogram rowstacked 
 	set style fill solid 1.00 border lt -1
+
 	set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
-	set style histogram rowstacked title textcolor lt -1
-	
-	plot 'averages.csv' u 5:xtic(1), for [i=6:21] '' u i t column(i)
+
+	plot 'averages.csv' u 5:xtic(1) lt rgb '#69D2E7',\
+	'' u 6 lt rgb '#E0E4CC' t column(6), \
+	'' u 7 lt rgb '#E0E4CC' t column(7), \
+	'' u 8 lt rgb '#E0E4CC' t column(8), \
+	'' u 9 lt rgb '#FA6900' t column(9), \
+	'' u 10 lt rgb '#E0E4CC' t column(10), \
+	'' u 11 lt rgb '#E0E4CC' t column(11), \
+	'' u 12 lt rgb '#E0E4CC' t column(12), \
+	'' u 13 lt rgb '#FA6900' t column(13), \
+	'' u 14 lt rgb '#E0E4CC' t column(14), \
+	'' u 15 lt rgb '#E0E4CC' t column(15), \
+	'' u 16 lt rgb '#FA6900' t column(16), \
+	'' u 17 lt rgb '#E0E4CC' t column(17), \
+	'' u 18 lt rgb '#E0E4CC' t column(18), \
+	'' u 19 lt rgb '#FA6900' t column(19), \
+	'' u 20 lt rgb '#E0E4CC' t column(20), \
+	'' u 21 lt rgb '#E0E4CC' t column(21)
 EOFMarker
--- logs/logs_partial_30/plot_profiling.sh
+++ logs/logs_partial_30/plot_profiling.sh
@@ -14,10 +14,26 @@ gnuplot -persist <<-EOFMarker
 	set xtics 1
 	
 	set style data histograms
-	
+	set style histogram rowstacked 
 	set style fill solid 1.00 border lt -1
-	set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
-	set style histogram rowstacked title textcolor lt -1
 	
-	plot 'averages.csv' u 5:xtic(1), for [i=6:21] '' u i t column(i)
+	set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
+	
+	plot 'averages.csv' u 5:xtic(1) lt rgb '#55B4B0',\
+	'' u 6 lt rgb '#EFC050' t column(6), \
+	'' u 7 lt rgb '#EFC050' t column(7), \
+	'' u 8 lt rgb '#EFC050' t column(8), \
+	'' u 9 lt rgb '#9B2335' t column(9), \
+	'' u 10 lt rgb '#EFC050' t column(10), \
+	'' u 11 lt rgb '#EFC050' t column(11), \
+	'' u 12 lt rgb '#EFC050' t column(12), \
+	'' u 13 lt rgb '#9B2335' t column(13), \
+	'' u 14 lt rgb '#EFC050' t column(14), \
+	'' u 15 lt rgb '#EFC050' t column(15), \
+	'' u 16 lt rgb '#9B2335' t column(16), \
+	'' u 17 lt rgb '#EFC050' t column(17), \
+	'' u 18 lt rgb '#EFC050' t column(18), \
+	'' u 19 lt rgb '#9B2335' t column(19), \
+	'' u 20 lt rgb '#EFC050' t column(20), \
+	'' u 21 lt rgb '#EFC050' t column(21)
 EOFMarker
