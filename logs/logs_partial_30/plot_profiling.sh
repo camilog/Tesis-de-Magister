b6265af6e71272089a11b1f18367f58423a54d4d
Camilo Gomez <cagomeznu@gmai.com>
2017-04-11 17:46:12 -0300

    termina contenidos

--- logs/logs_partial_30/plot_profiling.sh
+++ logs/logs_partial_30/plot_profiling.sh
@@ -19,21 +19,21 @@ gnuplot -persist <<-EOFMarker
 	
 	set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
 	
-	plot 'averages.csv' u 5:xtic(1) lt rgb '#55B4B0',\
-	'' u 6 lt rgb '#EFC050' t column(6), \
-	'' u 7 lt rgb '#EFC050' t column(7), \
-	'' u 8 lt rgb '#EFC050' t column(8), \
-	'' u 9 lt rgb '#9B2335' t column(9), \
-	'' u 10 lt rgb '#EFC050' t column(10), \
-	'' u 11 lt rgb '#EFC050' t column(11), \
-	'' u 12 lt rgb '#EFC050' t column(12), \
-	'' u 13 lt rgb '#9B2335' t column(13), \
-	'' u 14 lt rgb '#EFC050' t column(14), \
-	'' u 15 lt rgb '#EFC050' t column(15), \
-	'' u 16 lt rgb '#9B2335' t column(16), \
-	'' u 17 lt rgb '#EFC050' t column(17), \
-	'' u 18 lt rgb '#EFC050' t column(18), \
-	'' u 19 lt rgb '#9B2335' t column(19), \
-	'' u 20 lt rgb '#EFC050' t column(20), \
-	'' u 21 lt rgb '#EFC050' t column(21)
+	plot 'averages.csv' u 5:xtic(1) lt rgb '#69D2E7',\
+	'' u 6 lt rgb '#E0E4CC' t column(6), \
+	'' u 7 lt rgb '#E0E4CC' t column(7), \
+	'' u 8 lt rgb '#E0E4CC' t column(8), \
+	'' u 9 lt rgb '#FA6900' t column(9), \
+	'' u 10 lt rgb '#E0E4CC' t column(10), \
+	'' u 11 lt rgb '#E0E4CC' t column(11), \
+	'' u 12 lt rgb '#E0E4CC' t column(12), \
+	'' u 13 lt rgb '#FA6900' t column(13), \
+	'' u 14 lt rgb '#E0E4CC' t column(14), \
+	'' u 15 lt rgb '#E0E4CC' t column(15), \
+	'' u 16 lt rgb '#FA6900' t column(16), \
+	'' u 17 lt rgb '#E0E4CC' t column(17), \
+	'' u 18 lt rgb '#E0E4CC' t column(18), \
+	'' u 19 lt rgb '#FA6900' t column(19), \
+	'' u 20 lt rgb '#E0E4CC' t column(20), \
+	'' u 21 lt rgb '#E0E4CC' t column(21)
 EOFMarker
