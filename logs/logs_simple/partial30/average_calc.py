aa526cd1f795f4e4ba5fdd357da7ad7698ad629c
Camilo Gomez <cagomeznu@gmai.com>
2017-05-31 16:38:24 -0400

    Añade tiempos sin anonimato

---
+++ logs/logs_simple/partial30/average_calc.py
@@ -0,0 +1,15 @@
+import csv
+
+averages = open('averages.csv', 'w+')
+averages.write('Messages;Total Time (sec)\n')
+for i in range(1,31):
+	averages.write(str(i))
+	file_name = str(i) + '.csv'
+	for j in range(0, 1):
+		with open(file_name, newline='') as f:
+			reader = csv.reader(f, delimiter=';')
+			the_numbers = [float(row[j]) for row in reader]
+			average = sum(the_numbers) / len(the_numbers)
+			# averages.write(';' + str(average))
+			averages.write(';' + "%.4f" % average)
+	averages.write('\n')
